59a22402a1fa9ec8b7cda3f2a01334af7015a469
shabshaik13 <2021wa86762@wilp.bits-pilani.ac.in>
2025-07-14 15:13:40 +0530

    Updated greeting message in App.java

--- rocky-ci-demo/src/main/java/com/example/App.java
+++ rocky-ci-demo/src/main/java/com/example/App.java
@@ -1,13 +1,11 @@
-package com.example;
+package com.example; 
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
+public class App { 
+
+    public static void main(String[] args) { 
+
+        System.out.println("Hello from Jenkins CI/CD Pipeline!"); 
+
+    } 
+
+} 
